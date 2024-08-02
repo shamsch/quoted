153fcab38ae20c7d27a3a6864ab2e1347908d9be
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2024-08-02 16:06:14 +0300

    change id to generated value

--- src/main/kotlin/fi/shams/quoted/model/Quote.kt
+++ src/main/kotlin/fi/shams/quoted/model/Quote.kt
@@ -1,17 +1,15 @@
 package fi.shams.quoted.model
 
-import jakarta.persistence.CascadeType
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.Id
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.ManyToOne
-import jakarta.persistence.Table
+import jakarta.persistence.*
 
 @Entity
 @Table(name = "quotes")
 data class Quote(
-    @Id @Column(name = "id") val id: String,
+    @Id @Column(name = "id") @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "quote_id_seq"
+    )
+    val id: String,
 
     @Column(name = "quote") val quote: String,
 
